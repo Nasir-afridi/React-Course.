fb0009624b74759d70fdec6ac307b346fc6fcb08
Nasir-afridi <nasirwc05@gmail.com>
2024-06-23 15:49:35 +0500

    Chapter-01,02 added of react & redux

---
+++ 02-create-react-component/create-first-comp/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>this is the best react course</h1>
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
